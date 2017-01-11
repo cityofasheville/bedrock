783c7bd85aef45f91a7c7f2698322d2aca8508c2
Dave Michelson <daveism@gmail.com>
2017-01-11 16:18:10 -0500

    Ran the first FME job

--- scripts/jobrunner/run_job.js
+++ scripts/jobrunner/run_job.js
@@ -6,17 +6,22 @@ let fd = fs.openSync(`${process.argv[2]}/status.json`, 'r');
 let status = JSON.parse(fs.readFileSync(fd, { encoding: 'utf8' }));
 fs.closeSync(fd);
 status.status = 'Running';
+console.log(`The job info is ${JSON.stringify(status.job)}`);
 fd = fs.openSync(`${process.argv[2]}/status.json`, 'w');
 fs.writeFileSync(fd, JSON.stringify(status), { encoding: 'utf8' });
 fs.closeSync(fd);
 
 
 let path =  './sleeper.bat';
-const bat = require.resolve(path);
+let bat = require.resolve(path);
 const options = { detached: false, shell: false };
-
+let args = [5, process.argv[2]];
+if (status.job.type === 'fme') {
+  bat = `fme`;
+  args = [status.job.path];
+}
 console.log(new Date());
-const run = spawn(bat, [5, process.argv[2]], options);
+const run = spawn(bat, args, options);
 
 run.on('close', (code) => {
   console.log(`The child process ended with code ${code}`);

0edc8cb8aed53aa79d35aaa59655644dea077988
Eric Jackson <eric@deepweave.com>
2018-06-11 09:41:15 -0400

    Adding a report-status function

---
+++ scripts/report_status.js
@@ -0,0 +1,55 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+
+let transporter = nodemailer.createTransport({
+    host: '192.168.0.8',
+});
+
+const workDir = 'C:/COA/etl_jobs_dir';
+const files = fs.readdirSync(workDir);
+
+if (files.indexOf('jobs_status.json') < 0) {
+  logger.error(`Unable to find jobs_status.json in jobs directory ${workDir}`);
+  return console.log(`Unable to find jobs_status.json in jobs directory ${workDir}`);
+}
+const fd = fs.openSync(`${workDir}/jobs_status.json`, 'r');
+const jTracker = JSON.parse(fs.readFileSync(fd, { encoding: 'utf8' }));
+console.log
+fs.closeSync(fd);
+
+const errors = jTracker.errored.length;
+const unfinished = jTracker.sequencedToDo.length +
+    jTracker.freeToDo.length + jTracker.running.length;
+const completed = jTracker.completed.length;
+const status = (errors + unfinished > 0) ? 'ERROR' : 'OK';
+const emailText = `
+Final status: ${status}
+
+Total errors: ${errors}
+Total unfinished: ${unfinished}
+Total completed: ${completed}
+
+Detailed job results:
+
+${Object.keys(jTracker.jobStatus).map(itm => {
+    return `${itm}: ${jTracker.jobStatus[itm]}`;
+}).join('\n')}
+`;
+
+console.log(emailText);
+let mailOptions = {
+    from: 'dataserviceaccount@ashevillenc.gov',
+    to: 'ejackson@ashevillenc.gov',
+    subject: `ETL Jobs Status: ${status}`,
+    text: emailText,
+};
+
+const doit = true;
+if (doit) {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(`Message sent: ${info.messageId}`);
+    });
+}

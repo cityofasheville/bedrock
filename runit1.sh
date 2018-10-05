83e5c89929f5f1e7bf6fa778c1f6a87809a8d7ff
Eric Jackson <eric@deepweave.com>
2018-10-05 09:46:54 -0400

    upgraded version of pg package

--- runit1.sh
+++ runit1.sh
@@ -2,6 +2,6 @@
 
 # start in C:/coa/complexcitycoa
 
-node C:/coa/complexcity/scripts/traverse_and_do_task.js etl --start=C:/coa/managed-data-assets/everbridge --dest=C:/coa/etl_jobs_dir --logfile=C:/coa/etl_jobs_dir/joblog.log
+node C:/coa/complexcity/scripts/traverse_and_do_task.js etl --start=C:/coa/managed-data-assets --dest=C:/coa/etl_jobs_dir --logfile=C:/coa/etl_jobs_dir/joblog.log
 
 # node C:/coa/complexcitycoa/scripts/run_etl_jobs.js --logfile=C:/coa/etl_jobs_dir/etl.log C:/coa/etl_jobs_dir
---
+++ runloaddb.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+# start in C:/coa/complexcity
+
+node C:/coa/complexcity/scripts/traverse_and_do_task.js loaddb --require dotenv/config  dotenv_config_path=C:/coa/complexcity/.env --start=C:/jon/managed-data-assets

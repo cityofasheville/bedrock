76a1849af60befbfea47327404b4317f7e60e2b0
Eric Jackson <eric@deepweave.com>
2018-10-16 09:48:43 -0400

    set up package.json to use as devel environment and library

--- runit1.sh
+++ runit1.sh
@@ -2,4 +2,4 @@
 
 # start in C:/jon/complexcity
 
-node C:/jon/complexcity/scripts/traverse_and_do_task.js etl --start=C:/jon/managed-data-assets/everbridge/Customers --dest=C:/jon/etl_jobs_dir --logfile=C:/jon/etl_jobs_dir/joblog.log
+node C:/jon/complexcity/scripts/traverse_and_do_task.js etl --start=C:/jon/managed-data-assets --dest=C:/jon/etl_jobs_dir --logfile=C:/jon/etl_jobs_dir/joblog.log

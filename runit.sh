a00146706ca01cc199d1d4cab31288ebe4d04b10
Eric Jackson <eric@deepweave.com>
2018-10-16 10:20:53 -0400

    remove runit scripts from repo

--- runit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-# start in C:/jon/complexcity
-
-node C:/jon/complexcity/scripts/run_etl_jobs.js --logfile=C:/jon/etl_jobs_dir/etl.log C:/jon/etl_jobs_dir
--- runit1.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-# start in C:/jon/complexcity
-
-node C:/jon/complexcity/scripts/traverse_and_do_task.js etl --start=C:/jon/managed-data-assets --dest=C:/jon/etl_jobs_dir --logfile=C:/jon/etl_jobs_dir/joblog.log

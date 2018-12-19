40de08455799d1f5a0914faf2b9ac358fb1d41f4
Eric Jackson <eric@deepweave.com>
2018-12-19 11:49:08 -0500

    12/19

--- bin/bedrock.js
+++ bin/bedrock.js
@@ -16,6 +16,11 @@ if (args.argCount() < 1) usageAndExit();
 
 const command = args.getArg(0);
 
+console.log('start',args.getOption('start', 'none'));
+console.log("command: ", command);
+console.log("env:db1host: ", process.env.db1host)
+console.log("Current dir: ", process.cwd(), __dirname);
+
 if(command==="version"){
     console.log(bedrock.version());
 }else if(command==="checkout"){

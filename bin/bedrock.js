39863fbb4c5aa478cb7c6eca951fdb7877540621
Eric Jackson <ejackson@ashevillenc.gov>
2019-01-17 15:14:00 -0500

    Fixed linting errors

--- bin/bedrock.js
+++ bin/bedrock.js
@@ -1,43 +1,49 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
-require('dotenv').config({ path: 'c:/coa/bedrock/.env' })
-const { stripPath } = require('../scripts/common/utilities');
-
-let bedrock = require('../index.js');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const bedrock = require('../index.js');
 const CommandLineArgs = require('../scripts/common/CommandLineArgs');
 const checkout = require('../scripts/checkout');
 const report = require('../scripts/report_status');
-const traverse_and_do_task = require('../scripts/traverse_and_do_task');
-const create_asset = require('../scripts/create_asset');
-const create_schema = require('../scripts/create_schema');
-const run_etl_jobs = require('../scripts/run_etl_jobs');
+const traverseAndDoTask = require('../scripts/traverse_and_do_task');
+const createAsset = require('../scripts/create_asset');
+const createSchema = require('../scripts/create_schema');
+const runEtlJobs = require('../scripts/run_etl_jobs');
 const args = new CommandLineArgs(process.argv.slice(2));
 if (args.argCount() < 1) usageAndExit();
 
+if (fs.existsSync('./.env')) {
+  dotenv.config({ path: './.env' });
+} else {
+  dotenv.config({ path: 'c:/coa/bedrock/.env' });
+}
 const command = args.getArg(0);
 
 // console.log('start',args.getOption('start', 'none'));
-// console.log("command: ", command);
-// console.log("env:db1host: ", process.env.db1host)
-// console.log("Current dir: ", process.cwd(), __dirname);
+// console.log('command: ', command);
+// console.log('env:db1host: ', process.env.db1host)
+// console.log('Current dir: ', process.cwd(), __dirname);
 
-if(command==="version"){
-    console.log(bedrock.version());
-}else if(command==="checkout"){
-    checkout().catch(e => {console.error('query error', e.message, e.stack);});
-}else if(command==="checkin" || command==="init_etl" || command==="list"){
-    traverse_and_do_task();
-}else if(command==="create-asset"){
-    create_asset();
-}else if(command==="create-schema"){
-    create_schema();
-}else if(command==="run_etl"){
-    run_etl_jobs();
-}else if(command==="report"){
-    report();
+
+if (command === 'version') {
+  console.log(bedrock.version());
+} else if (command === 'checkout') {
+  checkout().catch(e => { console.error('query error', e.message, e.stack); });
+} else if (command === 'checkin' || command === 'init_etl' || command === 'list') {
+  traverseAndDoTask();
+} else if (command === 'create-asset') {
+  createAsset();
+} else if (command === 'create-schema') {
+  createSchema();
+} else if (command === 'run_etl') {
+  runEtlJobs();
+} else if (command === 'report') {
+  report();
 }
 
-function usageAndExit(){
-    console.log(`Usage:\tbedrock [checkin|checkout|init_etl|run_etl|report|create|version]`);
-    process.exit(1);
+function usageAndExit() {
+  console.log('Usage:\tbedrock [checkin|checkout|init_etl|run_etl|report|create-asset|version]');
+  process.exit(1);
 }
